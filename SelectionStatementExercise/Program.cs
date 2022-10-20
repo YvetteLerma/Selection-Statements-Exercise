15ff1874ed446dd65417e5182142057bbe116fe1
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-20 08:20:11 -0600

    code has been added

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is my favorite number?");
+
+            var r = new Random();
+            var favNumber = r.Next(1, 50);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+            Console.WriteLine($"TOO LOW!");
+
+            }
+
+            else if (userInput > favNumber)
+            {
+            Console.WriteLine($"Too high");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed my favorite number!");
+            }
+
         }
     }
 }
