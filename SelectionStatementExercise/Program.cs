927045c3b7fadd811d372274d6de2ce317b893dc
Yvette Lerma <lerma.yvette@hotmail.com>
2023-09-13 10:21:39 -0600

    added counter to code execution

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -7,7 +7,8 @@
             var r = new Random();
             var favNumber = r.Next(1, 50);
             int userInput; // will have to initialize userInput outside of the loop in order for it to execute at the end
-            
+            var counter = 0; //will count how many times the code executes as long as you call it in the do while loop below
+
             do //do executes the code below // you do not have to add a do while, however, the code will only run once without it
             {
                 Console.WriteLine("Pick a number from 1 to 50.");
@@ -28,6 +29,10 @@
                 {
                     Console.WriteLine("You guessed my favorite number!");
                 }
+
+                counter++; //this will add the instance by one everytime the code is executed.
+
+                Console.WriteLine($"You have guessed {counter} times!"); //this will print the count after the code is executed
 
 
             } while (userInput != favNumber); //userInput does not equal favNumber //cannot declare something inside the do while and call it outside of the loop
