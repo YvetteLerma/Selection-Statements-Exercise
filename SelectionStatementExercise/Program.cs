03e31f0e210d6c9d77b9c2747136be5365d17b4f
Yvette Lerma <lerma.yvette@hotmail.com>
2023-09-13 07:12:11 -0600

    added additional comments for understanding

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -8,10 +8,10 @@
             var favNumber = r.Next(1, 50);
             int userInput; // will have to initialize userInput outside of the loop in order for it to execute at the end
             
-            do //do executes the code below
+            do //do executes the code below // you do not have to add a do while, however, the code will only run once without it
             {
                 Console.WriteLine("Pick a number from 1 to 50.");
-                userInput = int.Parse(Console.ReadLine());
+                userInput = int.Parse(Console.ReadLine()); //.Parse converts the int to text like Convert.ToInt32(Console.ReadLine());
 
                 if (userInput < favNumber)
                 {
