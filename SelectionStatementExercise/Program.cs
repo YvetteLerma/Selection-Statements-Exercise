ea5b15932c2ba1b268926e1b6de432dcf11fb186
Yvette Lerma <lerma.yvette@hotmail.com>
2023-09-13 07:10:06 -0600

    added a loop

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,28 +6,31 @@
         {
             var r = new Random();
             var favNumber = r.Next(1, 50);
-
-            Console.WriteLine("Pick a number from 1 to 50.");
-            var userInput = int.Parse(Console.ReadLine());
-
-            if (userInput < favNumber)
-            {
-            Console.WriteLine($"{userInput} IS TOO LOW!");
-
-            }
-
-            else if (userInput > favNumber)
-            {
-            Console.WriteLine($"{userInput} IS TOO HIGH!");
-
-            }
-            else
-            {
-                Console.WriteLine("You guessed my favorite number!");
-            }
-
+            int userInput; // will have to initialize userInput outside of the loop in order for it to execute at the end
             
-            }
+            do //do executes the code below
+            {
+                Console.WriteLine("Pick a number from 1 to 50.");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} IS TOO LOW!");
+
+                }
+
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"{userInput} IS TOO HIGH!");
+
+                }
+                else
+                {
+                    Console.WriteLine("You guessed my favorite number!");
+                }
+
+
+            } while (userInput != favNumber); //userInput does not equal favNumber //cannot declare something inside the do while and call it outside of the loop
         }
     }
-
+}
